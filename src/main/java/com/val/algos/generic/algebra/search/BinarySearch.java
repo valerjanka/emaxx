1ca4a6dd88be01107d41ed688d04b74bff8c8536
valerjanka <adas.ua@gmail.com>
2023-04-08 00:01:48 -0400

    Move BinarySearch and refactor test.

---
+++ src/main/java/com/val/algos/generic/algebra/search/BinarySearch.java
@@ -0,0 +1,33 @@
+package com.val.algos.generic.algebra.search;
+
+/**
+ * Created 4/11/2019
+ *
+ * @author valerjanka
+ */
+public class BinarySearch {
+    /**
+     * Search for element position in given sorted array
+     *
+     * @param array   to be searched. Should be sorted.
+     * @param element to search
+     * @return position of the result element or -1
+     */
+    public static int searchExist(int[] array, int element) {
+        int l = 0;
+        int r = array.length;
+        while (l < r) {
+            int m = (l + r) / 2;
+            int comparison = element - array[m];
+            if (comparison == 0) {
+                return m;
+            } else if (comparison > 0) { // on the right
+                l = m + 1; // exclusive
+            } else { // on the left
+                r = m; // exclusive
+            }
+        }
+        return -1;
+    }
+
+}
---
+++ src/test/java/com/val/algos/generic/algebra/search/BinarySearchTest.java
@@ -0,0 +1,33 @@
+package com.val.algos.generic.algebra.search;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author valerjanka
+ */
+public class BinarySearchTest {
+
+    @Test
+    public void searchExistInEvenArray() {
+        int[] array = new int[]{1, 2, 3, 4};
+        assertEquals(-1, BinarySearch.searchExist(array, 0));
+        assertEquals(0, BinarySearch.searchExist(array, 1));
+        assertEquals(1, BinarySearch.searchExist(array, 2));
+        assertEquals(2, BinarySearch.searchExist(array, 3));
+        assertEquals(3, BinarySearch.searchExist(array, 4));
+        assertEquals(-1, BinarySearch.searchExist(array, 5));
+    }
+
+    @Test
+    public void searchExistInOddArray() {
+        int[] array = new int[]{1, 2, 3};
+        assertEquals(-1, BinarySearch.searchExist(array, 0));
+        assertEquals(0, BinarySearch.searchExist(array, 1));
+        assertEquals(1, BinarySearch.searchExist(array, 2));
+        assertEquals(2, BinarySearch.searchExist(array, 3));
+        assertEquals(-1, BinarySearch.searchExist(array, 4));
+    }
+
+}
